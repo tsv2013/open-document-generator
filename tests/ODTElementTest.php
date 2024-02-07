6fae7315f1cfd7441122f9c6ef35c4fc19b95565
tsv2013 <tsv2013@noreply.github.com>
2024-02-07 11:42:00 +0300

    Fixed first u-test

--- tests/ODTElementTest.php
+++ tests/ODTElementTest.php
@@ -4,12 +4,11 @@ namespace OpenOfficeGenerator;
 
 use PHPUnit\Framework\TestCase;
 
-class ODTElementTest extends TestCase
-{
-    public function testConstructor()
-    {
+class ODTElementTest extends TestCase {
+    public function testConstructor() {
         $element = new ODTElement("test-namespace", "test-name");
-        $this->assertEquals("test-name", $element->name);
+        $this->assertEquals("<test-namespace:test-name ></test-namespace:test-name>", implode("", iterator_to_array($element->create(), false)));
+        $this->assertEquals(["<test-namespace:test-name >", "</test-namespace:test-name>"], iterator_to_array($element->create(), false));
     }
 }
 
