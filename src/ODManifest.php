31fe273c8b7684e60bc2fd9d737346cd21bd826b
tsv2013 <tsv2013@noreply.github.com>
2024-03-22 13:20:43 +0300

    Optimized manifest generation

--- src/ODManifest.php
+++ src/ODManifest.php
@@ -6,24 +6,13 @@ class ODManifest extends ODElement {
   function __construct() {
     parent::__construct("manifest", "manifest");
     $this->attributes = "xmlns:manifest=\"urn:oasis:names:tc:opendocument:xmlns:manifest:1.0\" manifest:version=\"1.2\"";
+    $this->add(new ODManifestFileEntry("application/binary", "layout-cache"));
+    $this->add(new ODManifestFileEntry("text/xml", "content.xml"));
+    $this->add(new ODManifestFileEntry("text/xml", "styles.xml"));
   }
   public function get_head() {
     yield "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
     yield from parent::get_head(false);
-  }
-  public function get_content() {
-    yield "<manifest:file-entry manifest:media-type=\"\" manifest:full-path=\"Configurations2/accelerator/current.xml\"/>
-    <manifest:file-entry manifest:media-type=\"application/vnd.sun.xml.ui.configuration\" manifest:full-path=\"Configurations2/\"/>
-    <manifest:file-entry manifest:media-type=\"image/png\" manifest:full-path=\"Thumbnails/thumbnail.png\"/>
-    <manifest:file-entry manifest:media-type=\"text/xml\" manifest:full-path=\"content.xml\"/>
-    <manifest:file-entry manifest:media-type=\"text/xml\" manifest:full-path=\"settings.xml\"/>
-    <manifest:file-entry manifest:media-type=\"text/xml\" manifest:full-path=\"styles.xml\"/>
-    <manifest:file-entry manifest:media-type=\"application/rdf+xml\" manifest:full-path=\"manifest.rdf\"/>
-    <manifest:file-entry manifest:media-type=\"text/xml\" manifest:full-path=\"meta.xml\"/>";
-    // yield <manifest:file-entry manifest:media-type=\"text/xml\" manifest:full-path=\"content.xml\"/>
-    //       <manifest:file-entry manifest:media-type=\"application/binary\" manifest:full-path=\"layout-cache\"/>
-    //       <manifest:file-entry manifest:media-type=\"text/xml\" manifest:full-path=\"styles.xml\"/>";
-    yield from parent::get_content();
   }
   public function add_file_entry($type, $full_path) {
     $file_entry = new ODManifestFileEntry($type, $full_path);
